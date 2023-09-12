2e63225aeb97cd1b115ef793f056cd2a6e7f0c28
NeuracleMaGuangxin <maguangxin@neuracle.cn>
2023-09-12 18:03:42 +0800

    要留清白在人间基本结构

---
+++ Helps/MessageHelp.cs
@@ -0,0 +1,27 @@
+﻿using HandyControl.Controls;
+
+namespace TuShan.CleanDeath.Helps
+{
+    public static class MessageHelp
+    {
+        #region message
+
+        public static void InfoMessageShow(string message)
+        {
+            Growl.Info(message, "InfoMessage");
+        }
+        public static void ErrorMessageShow(string message)
+        {
+            Growl.Error(message, "ErrorMessage");
+            return;
+        }
+        public static void ClearMessageShow()
+        {
+            HandyControl.Controls.Growl.Clear("InfoMessage");
+            HandyControl.Controls.Growl.Clear("ErrorMessage");
+            return;
+        }
+
+        #endregion
+    }
+}
